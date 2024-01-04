e74c786901021dfd79cf917649647c7df2f53973
Syam Narendra <syamnarendra85@gmail.com>
2024-01-04 18:04:23 +0530

    Filter urls with Regex

--- src/main.js
+++ src/main.js
@@ -1,23 +1,49 @@
-import { PlaywrightCrawler } from "crawlee";
+import { CheerioCrawler, Dataset } from "crawlee";
+import OpenAI from "openai";
+// import { config } from "dotenv";
+// config();
 
-const crawler = new PlaywrightCrawler({
-  async requestHandler({ $, request, page, enqueueLinks, log, pushData }) {
-    const title = await page.title();
-    log.info(`Title of ${request.loadedUrl} is '${title}'`);
+// const resource = process.env.resource;
+// const deployment = process.env.deployment;
+// const apiVersion = process.env.apiVersion;
+// const apiKey = process.env.apiKey;
 
-    // Save results as JSON to ./storage/datasets/default
-    await pushData({ title, url: request.loadedUrl });
+// const openai = new OpenAI({
+//   apiKey: apiKey,
+//   baseURL: `https://${resource}.openai.azure.com/openai/deployments/${deployment}`,
+//   defaultQuery: { "api-version": apiVersion },
+//   defaultHeaders: { "api-key": apiKey },
+// });
 
-    // Extract links from the current page
-    // and add them to the crawling queue.
-    // await enqueueLinks();
+const crawler = new CheerioCrawler({
+  maxRequestsPerCrawl: 100,
+
+  async requestHandler({ request, $, pushData, enqueueLinks, requestQueue }) {
+    const title = $("title").text();
+    const data = { title, url: request.loadedUrl };
+    pushData(data);
+
+    // const links = $("a[href]")
+    //   .map((_, el) => $(el).attr("href"))
+    //   .get();
+
+    // const absoluteUrls = links.map(
+    //   (link) => new URL(link, request.loadedUrl).href
+    // );
+
+    // const userPrompt = `from this ${absoluteUrls} array give me only useful links which are related to italent`;
+    // const response = await openai.chat.completions.create({
+    //   messages: [{ role: "system", content: userPrompt }],
+    //   model: "gpt-3.5-turbo",
+    // });
+    // console.log(response);
+    // await crawler.addRequests(absoluteUrls);
+
+    await enqueueLinks({
+      regexps: [/^(?!http(s)?:\/\/www\.google\.com\/).*/i],
+    });
   },
-
-  //   maxRequestsPerCrawl: 20,
-
-  headless: false,
 });
 
-await crawler.run([
-  "https://www.google.com/search?q=italent&sca_esv=595051483&sxsrf=AM9HkKkcjJZmPYoIplezmmkoslgOFqfX9g%3A1704191493694&source=hp&ei=BeaTZZLMKMKPseMPoqGD2A4&iflsig=AO6bgOgAAAAAZZP0FQrSepZkAmKIoRQ2EQ_Tq6sZYaJJ&oq=itale&gs_lp=Egdnd3Mtd2l6IgVpdGFsZSoCCAAyChAjGIAEGIoFGCcyBBAjGCcyBBAjGCcyCxAuGK8BGMcBGIAEMgUQABiABDIFEAAYgAQyBxAuGIAEGAoyBRAuGIAEMgUQABiABDILEC4YgAQYxwEYrwFIpSRQvgZY7BBwAXgAkAEAmAGRAaAB-gSqAQMwLjW4AQHIAQD4AQGoAgrCAgcQIxjqAhgnwgINEC4YxwEYrwEY6gIYJ8ICCxAAGIAEGLEDGIMBwgIREC4YgAQYsQMYgwEYxwEY0QPCAg4QABiABBiKBRixAxiDAcICFBAuGIAEGMcBGK8BGJgFGJ4FGJkFwgIIEAAYgAQYsQPCAggQLhixAxiABMICCxAuGIAEGLEDGIMB&sclient=gws-wiz#cobssid=s",
-]);
+await crawler.run(["https://www.google.com/search?q=realme"]);
+await Dataset.exportToJSON("items");
